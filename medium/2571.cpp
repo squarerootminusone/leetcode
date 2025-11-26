725e57e47228a17d8be3c47b065482466c7dfcbe
squarerootminusone <sqrtminones@gmail.com>
2025-11-27 00:04:58 +0100

    2571

---
+++ medium/2571.cpp
@@ -0,0 +1,18 @@
+#define min(a, b) (a < b ? a : b)
+
+class Solution {
+public:
+    int minOperations(int n) {
+        int res = 0;
+        while (n > 0) {
+            if ((n & 3) == 3) {
+                n++;
+                res++;
+            } else {
+                res += n & 1;
+                n >>= 1;
+            }
+        }
+        return res;
+    }
+};
